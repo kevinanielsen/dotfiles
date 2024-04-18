1371eed6414b14b8d0f2a74cc19d56a3b3a9db14
Kevin Nielsen <kevinanielsen@outlook.com>
2024-04-18 10:01:47 +0200

    update

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,21 @@
+alias v="nvim"
+alias vim="nvim"
+alias vi="nvim"
+
+alias ..="cd .."
+alias dot="cd ~/.dotfiles"
+alias c="clear"
+
+# ls
+alias ls="ls --color=auto"
+alias ll="ls -la"
+alias la="ls -lathr"
+
+# git
+alias g="git"
+alias gp="git pull"
+alias gs="git status"
+alias lg="lazygit"
+
+# supabase
+alias sb="supabase"
